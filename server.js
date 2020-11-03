6cb288724811054d7e103a343dce36e88194f239
Sarath Kumar <nagarajsarath@gmail.com>
2020-11-03 11:30:18 -0500

    Connect to DB
    Add default config
    Users route

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // Entry point ot our backend
 // NodeJs uses ES2015 modules unlike Front End Common JS
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Sample get
 app.get('/', (request, response) => response.json({ message: "Viola, What's up?" }));
 
+// Connect Database
+connectDB();
+// Initialize middleware
+app.use(express.json({ extende: false }));
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
