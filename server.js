8b3171054ed5cd6c4657ad46198271c0e70de959
Sarath Kumar <nagarajsarath@gmail.com>
2020-11-07 12:13:41 -0500

    remove unneeded get request

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
-// Sample get
-app.get('/', (request, response) => response.json({ message: "Viola, What's up?" }));
 
 // Connect Database
 connectDB();
 // Initialize middleware
-app.use(express.json({ extende: false }));
+app.use(express.json({ extended: false }));
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
