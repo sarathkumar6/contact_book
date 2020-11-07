d30568e9d4ec242568f18691b9138927127bd72a
Sarath Kumar <nagarajsarath@gmail.com>
2020-11-07 12:07:13 -0500

    Prepare for deploy version

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from '../contacts/ContactItem';
@@ -6,40 +6,47 @@ import ContactItem from '../contacts/ContactItem';
 const Contacts = (props) => {
 	const contactContext = useContext(ContactContext);
 
-	const { filtered, contacts } = contactContext;
+	const { filtered, contacts, getContacts } = contactContext;
 
-	!contacts && <h4>Pleae add a contact</h4>;
+	useEffect(() => {
+		getContacts();
+		//eslint-disable-next-line
+	}, []);
 
-	return (
-		<Fragment>
-			<TransitionGroup>
-				{filtered ? (
-					filtered.map((contact) => (
-						<CSSTransition key={contact.id} timeout={500} classNames='item'>
-							<ContactItem
-								phone={contact.phone}
-								type={contact.type}
-								email={contact.email}
-								id={contact.id}
-								name={contact.name}
-							/>
-						</CSSTransition>
-					))
-				) : (
-					contacts.map((contact) => (
-						<CSSTransition key={contact.id} timeout={500} classNames='item'>
-							<ContactItem
-								phone={contact.phone}
-								type={contact.type}
-								email={contact.email}
-								id={contact.id}
-								name={contact.name}
-							/>
-						</CSSTransition>
-					))
-				)}
-			</TransitionGroup>
-		</Fragment>
-	);
+	if (!contacts) {
+		return <h4>Pleae add a contact</h4>;
+	} else {
+		return (
+			<Fragment>
+				<TransitionGroup>
+					{filtered ? (
+						filtered.map((contact) => (
+							<CSSTransition key={contact._id} timeout={500} classNames='item'>
+								<ContactItem
+									phone={contact.phone}
+									type={contact.type}
+									email={contact.email}
+									id={contact._id}
+									name={contact.name}
+								/>
+							</CSSTransition>
+						))
+					) : (
+						contacts.map((contact) => (
+							<CSSTransition key={contact._id} timeout={500} classNames='item'>
+								<ContactItem
+									phone={contact.phone}
+									type={contact.type}
+									email={contact.email}
+									id={contact._id}
+									name={contact.name}
+								/>
+							</CSSTransition>
+						))
+					)}
+				</TransitionGroup>
+			</Fragment>
+		);
+	}
 };
 export default Contacts;
--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 // NodeJs uses ES2015 modules unlike Front End Common JS
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Sample get
-app.get('/', (request, response) => response.json({ message: "Viola, What's up?" }));
+app.get('/', (request, response) => response.json({}));
 
 // Connect Database
 connectDB();
@@ -16,4 +17,9 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (request, response) => response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 app.listen(PORT, () => console.log(`Viola, Server started on port ${PORT}`));
