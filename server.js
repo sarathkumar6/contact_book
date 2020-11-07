05c76cdac739cac28e607c8fb5137bbe51e67126
Sarath Kumar <nagarajsarath@gmail.com>
2020-11-07 12:19:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,4 +14,9 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-app.listen(PORT, () => console.log(`Viola, Server started on port ${PORT}`));
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (request, response) => response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
